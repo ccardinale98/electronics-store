dc0defec42ceeaa320db0fd6630ada4fb31620be
ccardinale98 <ccardinale98@gmail.com>
2021-10-16 13:29:56 -0400

    chatbot responds'

--- client/src/components/Bot/index.js
+++ client/src/components/Bot/index.js
@@ -10,44 +10,80 @@ import spinner from '../../assets/spinner.gif';
 function Bot() {
     const [message, setMessage] = useState("");
     
-    // function botConfig() {
-    //     console.log('Hi, I am your virtual assistant. Some commonly used commands include: \n"Explain X." \n"Show me products in X category." \n"Tell me more about X." \n"Contact a real person."')
-    //     if (input == "hello" || input == "hi" || input == "howdy") {
-    //         console.log("Hello!")
-    //     } else if (input.includes("how are you")) {
-    //         console.log("I am well! Thank you for asking.")
-    //     } else if (input == "hello" || input == "hi" || input == "howdy")
-    // }
     const handleClick = async (e) => {
        const code = e.keyCode || e.which;
        
        if (code === 13) {
            console.log(message);
+           appendMessage();
            setMessage("");
        }
     }
     
     function appendMessage() {
+        console.log(message)
         var ulEl = document.getElementById("message-list");
+
+        var mess = message.toLowerCase();
 
         if (message) {
             var userMess = document.createElement("li");
             userMess.innerHTML = message;
+            userMess.classList.add("message")
+            userMess.classList.add("user-message")
             ulEl.append(userMess);
         
             var botMess = document.createElement("li");
-            
+            botMess.classList.add("message")
+            botMess.classList.add("bot-message")
+
+            if (mess == "hi" || mess == "hello" || mess == "howdy") {
+                botMess.innerHTML = "Hello!";
+            } else if (mess.includes("how are you")) {
+                botMess.innerHTML = "I am well.";
+            } else if (mess.includes("category")) {
+                botMess.innerHTML = "These are the items in that category:";
+                // make if for each category (import categories)
+            } else if (mess.includes("tell") || mess.includes("explain")) {
+                // (import products) make if for the prodicts
+            } else if (mess.includes("contact") || mess.includes("person")) {
+                botMess.append(showContact())
+            } else if (mess.includes("bye") || mess.includes("goodbye")) {
+                botMess.innerHTML = "Bye!"
+            } else {
+                botMess.innerHTML = "Sorry, I did not catch that. I am still learning."
+            }
+
+            var wait = document.createElement("li");
+            wait.innerHTML = "...";
+            ulEl.append(wait);
+
+            setTimeout(
+                function () {
+                    ulEl.append(botMess);
+                    wait.remove();
+                }, 2000
+            )
         }  
     }
     
-    
-    
-    
-    
+    function showContact() {
+        return (
+            <div>
+                <p>Name: </p>
+                <input id="contact-name"></input>
+                <p>Email: </p>
+                <input id="contact-email"></input>
+                <p>Message: </p>
+                <input id="contact-message"></input>
+                <button id="contact-submit">Submit</button>
+            </div>
+        )
+    } 
     
     return (
-        <div>
-            <h1>Virtual Assistant</h1>
+        <div id="chat-box-div">
+            <h1 id="chat-box-title">Virtual Assistant</h1>
             <div>
                 <ul id="message-list">
                 </ul>
