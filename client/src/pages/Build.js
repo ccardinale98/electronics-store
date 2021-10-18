13222cd92245b407d993203dc43c2494e72789df
ccardinale98 <ccardinale98@gmail.com>
2021-10-18 00:11:09 -0400

    styling

--- client/src/pages/Build.js
+++ client/src/pages/Build.js
@@ -1,13 +1,15 @@
 import React from "react";
 import BuildMenu from "../components/BuildMenu";
-import { Link, useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Bot from "../components/Bot";
+import Cart from "../components/Cart";
 
 const Build = () => {
     return (
         <div className="container" id="build-main">
             <Link to="/" class="back-home">Home</Link>
             <BuildMenu />
+            <Cart />
             <Bot />
         </div>
     )
